d5a3b36eea0090408fbb6477ac6da69e51a5de92
Keks <keks@htmlacademy.ru>
2024-09-01 21:35:29 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/render-thumbs.js
@@ -0,0 +1,28 @@
+import { createArrayPhotoItems, PHOTO_QUANTITY } from './data.js';
+
+const thumbs = createArrayPhotoItems(PHOTO_QUANTITY);
+
+const template = document.querySelector('#picture').content.querySelector('.picture');
+const fragment = document.createDocumentFragment();
+const picContainer = document.querySelector('.pictures');
+
+const createThumbnail = (arrEl) => {
+  const thumbnail = template.cloneNode(true);
+  const pictureImg = thumbnail.querySelector('.picture__img');
+  thumbnail.querySelector('.picture__comments').textContent = arrEl.comments.length;
+  thumbnail.querySelector('.picture__likes').textContent = arrEl.likes;
+  thumbnail.dataset.pictureId = arrEl.id;
+  pictureImg.src = arrEl.url;
+  pictureImg.alt = arrEl.description;
+  return thumbnail;
+};
+
+const renderThumbs = () => {
+  thumbs.forEach((thumb) => {
+    fragment.appendChild(createThumbnail(thumb));
+  });
+  picContainer.appendChild(fragment);
+};
+
+
+export { renderThumbs, thumbs, picContainer };
---
+++ 7/js/show-big-picture.js
@@ -0,0 +1,74 @@
+import { thumbs } from './render-thumbs';
+
+const bigPictureSection = document.querySelector('.big-picture');
+const bigPictureImg = bigPictureSection.querySelector('.big-picture__img').querySelector('img');
+const bigPictureLikesCount = bigPictureSection.querySelector('.likes-count');
+const bigPictureCancel = bigPictureSection.querySelector('.big-picture__cancel');
+const bigPictureSocial = bigPictureSection.querySelector('.big-picture__social');
+const socialCommentCount = bigPictureSocial.querySelector('.social__comment-count');
+const socialCommentsLoader = bigPictureSocial.querySelector('.social__comments-loader');
+const socialCommentTotalCount = bigPictureSocial.querySelector('.social__comment-total-count');
+const socialCaption = bigPictureSocial.querySelector('.social__caption');
+const socialComments = bigPictureSocial.querySelector('.social__comments'); //Список социальных комментариев
+
+//Процедура заполнения социальными комментариями из подготовленных данных
+const insertSocialComments = (commentsSet) => {
+  for (let i = 0; i < commentsSet.length; i++) {
+    const li = document.createElement('li');
+    li.classList.add('social__comment');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    p.classList.add('social__text');
+    p.textContent = commentsSet[i].message;
+    img.classList.add('social__picture');
+    img.setAttribute('src', commentsSet[i].avatar);
+    img.setAttribute('alt', commentsSet[i].name);
+    img.setAttribute('width', '35');
+    img.setAttribute('height', '35');
+    li.append(img);
+    li.append(p);
+    socialComments.append(li);
+  }
+};
+
+const showBigPicture = (currentPicId) => {
+  const thumb = thumbs[currentPicId];
+  bigPictureImg.src = thumb.url;
+  socialCaption.textContent = thumb.dscrp;
+
+  //Количество лайков likes подставьте как текстовое содержание элемента .likes-count.
+  bigPictureLikesCount.textContent = thumb.likes;
+
+  document.body.classList.add('modal-open');
+
+  //Общее количество комментариев к фотографии comments подставьте как текстовое содержание
+  //элемента .social__comment-total-count.
+  socialCommentTotalCount.textContent = thumb.comments.length;
+
+  //После открытия окна спрячьте блоки счётчика комментариев .social__comment-count и загрузки новых комментариев
+  //.comments-loader, добавив им класс hidden, с ними мы разберёмся позже, в другом домашнем задании.
+  socialCommentCount.classList.add('hidden');
+  socialCommentsLoader.classList.add('hidden');
+
+  //Заполняет социальные комментарии под фотографией
+  socialComments.innerHTML = '';
+  insertSocialComments(thumb.comments);
+
+  bigPictureCancel.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    bigPictureSection.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+  }, { once: true });
+
+  document.addEventListener('keydown', (evt) => {
+    evt.preventDefault();
+    if (evt.key === 'Escape') {
+      bigPictureSection.classList.add('hidden');
+      document.body.classList.remove('modal-open');
+    }
+  }, { once: true });
+
+  bigPictureSection.classList.remove('hidden');
+};
+
+export { showBigPicture };

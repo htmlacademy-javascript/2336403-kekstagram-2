4b42fc32d2e2699098e0d3e530de75ae406f7bb2
Denis Gorbunov <volo_non_valeo@mail.ru>
2024-09-11 16:56:36 +0300

    ДОбÐобавляет логику валидации Ð

--- js/img-upload-form.js
+++ js/img-upload-form.js
@@ -7,9 +7,22 @@ const imgUploadCancelBtn = imgUploadForm.querySelector('.img-upload__cancel');
 const textHashtags = imgUploadForm.querySelector('.text__hashtags');
 const textDescription = imgUploadForm.querySelector('.text__description');
 
+//Константы для валидатора//
+
+  //хеш-тег не может состоять только из одной решётки;
+  //максимальная длина одного хэштега 20 символов, включая решётку;
+  //хэштеги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом;
+  //хэштеги разделяются пробелами;
+  //один и тот же хэштег не может быть использован дважды;
+  //нельзя указать больше пяти хэштегов;
+  //rules: 'Хэштег должен начинаеться с символа # (решётка) и содержать только буквы и числа'
+
+let errorMessage = '';
+const error = () => errorMessage;
+
+//Конец констант для валидатора//
 
 const closeUploadFormKeydown = (evt) => {
-  window.console.log(textHashtags === document.activeElement);
   if ((isEscKey(evt)) && !(textHashtags === document.activeElement || textDescription === document.activeElement)) {
     evt.preventDefault();
     closeUploadForm();
@@ -34,5 +47,54 @@ const openUploadForm = () => {
 const setImgUploadHandler = () => {
   imgUploadInput.addEventListener('change', openUploadForm);
 };
+
+//Блок валидации//
+
+
+const isHashtagsValid = (inputStr) => {
+  inputStr = inputStr.toLowerCase().trim();
+  //errorMessage = '';
+  if (!inputStr) {
+    return true;
+  }
+  const hashtagsArray = inputStr.split(/\s+/);
+
+
+
+  const requirements = [
+    {
+      check: hashtagsArray.some((item) => item === '#'),
+      error: 'Хештег не может состоять только из одной решетки'
+    },
+    {
+      check: hashtagsArray.some((item) => item.slice(1).includes('#')),
+      error: 'Хештеги разделяются пробелами'
+    },
+  ];
+
+  window.console.log(errorMessage);
+
+  return requirements.every((requirement) => {
+    const isNotCompleted = requirement.check;
+    if (isNotCompleted) {
+      errorMessage = requirement.error;
+    }
+    return !isNotCompleted;
+  });
+
+};
+
+const pristine = new Pristine(imgUploadForm, {
+  classTo: 'img-upload__field-wrapper', // Элемент, на который будут добавляться классы
+  errorTextParent: 'img-upload__field-wrapper', // Элемент, куда будет выводиться текст с ошибкой
+  errorTextTag: 'p', // Тег, который будет обрамлять текст ошибки
+  errorTextClass: 'img-upload__field-wrapper--error' // Класс для элемента с текстом ошибки
+});
+
+pristine.addValidator(textHashtags, isHashtagsValid, errorMessage);
+
+
+//Конец блока валидации//
+
 
 export { setImgUploadHandler };
---
+++ js/validation-img-upload-form.js
@@ -0,0 +1,18 @@
+//const imgUploadForm = document.getElementById('img-upload__form');
+
+//const pristine = new Pristine(imgUploadForm);
+
+//window.console.log(pristine);
+
+// new Pristine(imgUploadForm);
+//
+// imgUploadForm.addEventListener('submit', (evt) => {
+//   evt.preventDefault();
+//
+//   const isValid = pristine.validate();
+//   if (isValid) {
+//     console.log('Можно отправлять');
+//   } else {
+//     console.log('Форма невалидна');
+//   }
+// });

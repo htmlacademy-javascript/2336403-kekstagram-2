6a33c35fa7e5d11fe49a29bce40ca03d17b0a485
Denis Gorbunov <volo_non_valeo@mail.ru>
2024-10-15 23:47:17 +0300

    удаляет ненужные комментарии

--- js/img-filters.js
+++ js/img-filters.js
@@ -3,15 +3,7 @@ import { renderThumbs } from './render-thumbs.js';
 const imgFilters = document.querySelector('.img-filters');
 const imgFiltersForm = imgFilters.querySelector('.img-filters__form');
 
-//imgFilters.classList.remove('img-filters--inactive');
-
 let localData;
-
-// const FILTER = {
-//   default: 'filter-default',
-//   random: 'filter-random',
-//   discussed: 'filter-discussed',
-// };
 
 const filteredData = {
   'filter-default': () => localData,
@@ -39,68 +31,3 @@ const initFilter = (thumbs) => {
 
 
 export { initFilter };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Filters = {
-//   DEFAULT: 'filter-default',
-//   RANDOM: 'filter-random',
-//   DISCUSSED: 'filter-discussed'
-// };
-
-//let localData;
-
-// const filteredData = {
-//   [Filters.DEFAULT]: () => localData,
-//   [Filters.RANDOM]: () => [...localData].sort(()=> Math.random() - 0.5).slice(0, 10),
-//   [Filters.DISCUSSED]: () => [...localData].sort((b, a) => a.comments.length - b.comments.length),
-// };
-
-// const makeButtonActive = (element) => {
-//   document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
-//   element.classList.add('img-filters__button--active');
-// };
-
-// const sortFilter = (evt) => {
-//   if (evt.target.classList.contains('img-filters__button')) {
-//     renderMini(filteredData[evt.target.id]());
-//   }
-// } ;
-
-// imgFiltersForm.addEventListener('click', debounce(sortFilter, DELAY_TIMER));
-// imgFiltersForm.addEventListener('click', (evt) => {
-//   if (evt.target.classList.contains('img-filters__button')) {
-//     makeButtonActive(evt.target);
-//   }
-// });
-
-// const activateFilters = (pictures) => {
-//   filters.classList.remove('img-filters--inactive');
-//   localData = [...pictures];
-// };

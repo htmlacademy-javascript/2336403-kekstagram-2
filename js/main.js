137bf3c254285e25628f92cc7ea9ff91c3ce2ee6
Denis Gorbunov <volo_non_valeo@mail.ru>
2024-10-26 09:22:50 +0300

    Добавляет функциональность загрузки пользовательского изображения

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { createGallery } from './create-gallery.js';
-import { setImgUploadHandler } from './img-upload-form.js';
+import { setImgUploadHandler, initPictureUpload } from './img-upload-form.js';
 import { getData } from './api.js';
 import { initFilter } from './img-filters.js';
 
@@ -7,6 +7,6 @@ const thumbs = await getData();
 if (thumbs.length !== 0) {
   initFilter(thumbs);
   createGallery(thumbs);
+  setImgUploadHandler();
+  initPictureUpload();
 }
-
-setImgUploadHandler();
